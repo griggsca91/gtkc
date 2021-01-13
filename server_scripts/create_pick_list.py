37816eb4a9caf5e6b18cdc112f03c03693babe6d
Chris <griggsca91@users.noreply.github.com>
2021-01-12 22:43:15 -0500

    Update create_pick_list.py

--- server_scripts/create_pick_list.py
+++ server_scripts/create_pick_list.py
@@ -1,4 +1,27 @@
 frappe.msgprint("testing");
-frappe.msgprint(json.dumps(frappe.form_dict))
+#frappe.msgprint(json.dumps(frappe.form_dict))
+
+doc_string = frappe.form_dict["doc"]
+
+doc = json.loads(doc_string)
+
+gifts = []
+
+frappe.msgprint(json.dumps(doc["gifts"]))
+
+for raw_gift in doc["gifts"]:
+    gift = {
+      "item_code": raw_gift["item_code"],
+      "qty": raw_gift["qty"],
+      "warehouse": raw_gift["warehouse"],
+    }
+    frappe.msgprint(gift["item_code"])
+    frappe.msgprint(gift["qty"])
+    frappe.msgprint(gift["warehouse"])
+
+
+customer = doc["customer"]
+
+frappe.msgprint(doc["order_type"])
 
 frappe()()
